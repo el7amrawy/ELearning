082ece299d8ffdd39b0c2937a5aa2ea4cb3224fc
Aly <el7amrawy@outlook.com>
2025-03-12 14:05:05 +0200

    add basic crud to the base repository

--- ELearning.Core/Interfaces/Repositories/IBaseRepository.cs
+++ ELearning.Core/Interfaces/Repositories/IBaseRepository.cs
@@ -1,7 +1,14 @@
-﻿namespace ELearning.Core.Interfaces.Repositories
+﻿using System.Linq.Expressions;
+
+namespace ELearning.Core.Interfaces.Repositories
 {
-	public interface IBaseRepository<Model> where Model : class
+	public interface IBaseRepository<Entity> where Entity : class
 	{
-		public Task<IEnumerable<Model>> GetAllAsync();
+		public Task<IEnumerable<Entity>> GetAllAsync(Expression<Func<Entity, bool>> criteria = null, string[] includes = null, int pageNumber = 0, int pageSize = 0);
+		public Task<Entity> GetByIdAsync(int id);
+		public Task<Entity> GetItemAsync(Expression<Func<Entity, bool>> criteria, string[] includes = null);
+        public void Add(Entity entity);
+		public void Update(Entity entity);
+		public void Delete(Entity entity);
 	}
 }
--- ELearning.EF/Repositories/BaseRepository.cs
+++ ELearning.EF/Repositories/BaseRepository.cs
@@ -1,17 +1,46 @@
-﻿using ELearning.Core.Interfaces.Repositories;
+﻿using System.Linq.Expressions;
+using ELearning.Core.Interfaces.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace ELearning.EF.Repositories
 {
-	public class BaseRepository<Model> : IBaseRepository<Model> where Model : class
+	public class BaseRepository<Entity> : IBaseRepository<Entity> where Entity : class
 	{
 		private readonly AppDbContext _db;
+        public BaseRepository(AppDbContext db) => _db = db;
+        public async Task<Entity> GetByIdAsync(int id) => await _db.Set<Entity>().FindAsync(id);
+        public void Add(Entity entity) => _db.Entry(entity).State = EntityState.Added;
+        public void Update(Entity entity) => _db.Entry(entity).State = EntityState.Modified;
+        public void Delete(Entity entity) => _db.Entry(entity).State = EntityState.Deleted;
+        public virtual async Task<IEnumerable<Entity>> GetAllAsync(Expression<Func<Entity, bool>> criteria = null, string[] includes = null, int pageNumber = 0, int pageSize = 0)
+        {
+            var query = _db.Set<Entity>().AsQueryable();
 
-		public BaseRepository(AppDbContext db)
-		{
-			_db = db;
-		}
+            if (criteria != null)
+                query = query.Where(criteria);
 
-		public async Task<IEnumerable<Model>> GetAllAsync() =>await _db.Set<Model>().ToListAsync();
-	}
+            if (includes != null)
+                foreach (var item in includes)
+                    query = query.Include(item);
+
+            if (pageNumber > 0)
+                query = query.Skip((pageNumber - 1) * pageSize);
+
+            if (pageSize > 0)
+                query = query.Take(pageSize);
+
+            return await query.ToListAsync();
+        }
+
+        public Task<Entity> GetItemAsync(Expression<Func<Entity, bool>> criteria, string[] includes = null)
+        {
+            var query = _db.Set<Entity>().AsQueryable();
+
+            if (includes != null)
+                foreach (var item in includes)
+                    query = query.Include(item);
+
+            return query.FirstOrDefaultAsync(criteria);
+        }
+    }
 }
