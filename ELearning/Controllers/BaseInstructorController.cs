b9f148d7a2b27a0ea522c8fe65db7b1339fc6adc
Aly <el7amrawy@outlook.com>
2025-03-30 17:32:01 +0200

    add instructor base controller

---
+++ ELearning/Controllers/BaseInstructorController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ELearning.Controllers
+{
+    [Authorize(Roles ="Instructor"),Route("Instructor/[Controller]/{Action=Index}/{Id?}")]
+    public class BaseInstructorController : Controller
+    {
+    }
+}
