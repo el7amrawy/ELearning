8eaa14821915ddf975c8d66bbda313e48c63fdf5
Aly <el7amrawy@outlook.com>
2025-03-30 16:47:45 +0200

    dashboard --> add create course page

--- ELearning/Areas/Dashboard/Controllers/CoursesController.cs
+++ ELearning/Areas/Dashboard/Controllers/CoursesController.cs
@@ -1,13 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ELearning.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ELearning.Areas.Dashboard.Controllers
 {
     public class CoursesController : BaseDashboardController
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CoursesController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult Create() => View();
+        public async Task<IActionResult> Create()
+        {
+            var levels = await _unitOfWork.Levels.GetAllAsync();
+            ViewBag.Levels = new SelectList(levels, "Id", "Name");
+
+            var langs = await _unitOfWork.Languages.GetAllAsync();
+            ViewBag.Languages = new SelectList(langs, "Id", "Name");
+
+            var status = await _unitOfWork.CoursesStatus.GetAllAsync();
+            ViewBag.Status = new SelectList(status, "Id", "Name");
+            return View();
+        }
     }
 }
