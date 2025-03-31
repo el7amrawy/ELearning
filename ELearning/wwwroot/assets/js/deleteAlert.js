5873e900a73130d9a7d7ec2de0c2b9fa9159bee4
Aly <el7amrawy@outlook.com>
2025-03-31 17:09:44 +0200

    dashboard --> add delete alert

---
+++ ELearning/wwwroot/assets/js/deleteAlert.js
@@ -0,0 +1,16 @@
+﻿function confirmDelete(id, name,url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: `You are about to delete "${name}" with Id: "${id}". This action cannot be undone.`,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'Cancel'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = url;
+        }
+    });
+}
