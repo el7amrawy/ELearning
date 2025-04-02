0d9515ab44657d6b2c2dd1d43de96bb508f9f53c
Aly <el7amrawy@outlook.com>
2025-04-02 03:23:05 +0200

    add enums

---
+++ ELearning.Core/Enums/CourseStatusEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace ELearning.Core.Enums
+{
+    public enum CourseStatusEnum
+    {
+        Published = 1,
+        Draft,
+        Canceled
+    }
+}
---
+++ ELearning.Core/Enums/LevelsEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace ELearning.Core.Enums
+{
+    public enum LevelsEnum
+    {
+        Beginner = 1,
+        Intermediate,
+        Advanced
+    }
+}
