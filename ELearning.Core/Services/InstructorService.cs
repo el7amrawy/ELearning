185750ea1a3d2a3e16c66428a2a0af57025af5e0
Aly <el7amrawy@outlook.com>
2025-04-03 05:45:46 +0200

    update error message in instructor service

--- ELearning.Core/Services/InstructorService.cs
+++ ELearning.Core/Services/InstructorService.cs
@@ -14,12 +14,14 @@ namespace ELearning.Core.Services
 
         public async Task<ServiceResult> ValidateCourseAsync(int instructorId, int courseId)
         {
+            if (courseId == 0) return ServiceResult.Failure($"course does not exist");
+
             var ids = await _unitOfWork.Courses.GetInstructorCoursesIdsAsync(instructorId);
 
             if (ids.Contains(courseId))
                 return ServiceResult.Success();
 
-            return ServiceResult.Failure("You don't have access to this course");
+            return ServiceResult.Failure("You are not allowed access to this course");
         }
     }
 }
