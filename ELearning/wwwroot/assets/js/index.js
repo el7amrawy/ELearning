3e40841144f328284dfd08d0cfbad52b9efc0ef4
Aly <el7amrawy@outlook.com>
2025-04-07 08:20:33 +0200

    update main navbar & add favicon

--- ELearning/wwwroot/assets/js/index.js
+++ ELearning/wwwroot/assets/js/index.js
@@ -1,87 +1,71 @@
-let menu = document.querySelector("#mobile-menu");
-let icon_menu = document.querySelector(".menu-icon")
-let user_menu = document.querySelector("[role=menu]")
-let user_icon = document.querySelector("#user-menu-button");
 let search_icon = document.querySelector(".search_icon");
 let search_input = document.querySelector(".search_input");
 let other_search = document.querySelector(".other-search");
 
 /* ===============>>> btn Open Menu <<<=============== */
-if(icon_menu){
-    icon_menu.addEventListener('click',function(){
-        if(search_input.children[0].className === "active"){
-            search_input.classList.toggle("hidden");
-            search_input.children[0].classList.toggle("active");
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+//console.log(mobileMenuButton.children)
+if (mobileMenuButton) mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+    Array.from(mobileMenuButton.children).forEach(ele => ele.classList.toggle("hidden"))
+});
+
+document.addEventListener('click', (event) => {
+    if (mobileMenu && mobileMenuButton) {
+        if (!mobileMenu.contains(event.target) && !mobileMenuButton.contains(event.target)) {
+            if (!mobileMenu.classList.contains("hidden")) Array.from(mobileMenuButton.children).forEach(ele => ele.classList.toggle("hidden"))
+            mobileMenu.classList.add('hidden');
         }
-        if(user_icon.parentElement.parentElement.children[1].children[0].className === "active"){
-            user_icon.parentElement.parentElement.children[1].children[0].classList.toggle("active");
-            user_menu.classList.toggle("hidden");
-        }
-        menu.classList.toggle("hidden");
-        menu.children[0].classList.toggle("active");
-        icon_menu.childNodes[5].classList.toggle("hidden");
-        icon_menu.childNodes[7].classList.toggle("hidden");
-    })
-}
-/* ===============>>> btn Open User <<<=============== */
-user_icon.addEventListener('click' ,function(){
-    if(search_input.children[0].className === "active"){
-        search_input.classList.toggle("hidden");
-        search_input.children[0].classList.toggle("active");
     }
-    if(menu.children[0].className === "active"){
-        menu.classList.toggle("hidden");
-        icon_menu.childNodes[5].classList.toggle("hidden");
-        icon_menu.childNodes[7].classList.toggle("hidden");
-        menu.children[0].classList.toggle("active");
+});
+// Toggle user dropdown
+const userMenuButton = document.getElementById('user-menu-button');
+const userDropdown = document.getElementById('user-dropdown');
+
+userMenuButton.addEventListener('click', () => {
+    if (userDropdown) userDropdown.classList.toggle('hidden');
+});
+
+// Close dropdown when clicking outside
+document.addEventListener('click', (event) => {
+    if (!userMenuButton.contains(event.target) && !userDropdown.contains(event.target)) {
+        userDropdown.classList.add('hidden');
     }
-    user_icon.parentElement.parentElement.children[1].children[0].classList.toggle("active");
-    user_menu.classList.toggle("hidden");
-})
+});
+
 /* ==============>>> btn open search <<<============== */
-search_icon.addEventListener('click' , function(){
-    if(menu.children[0].className === "active"){
-        menu.classList.toggle("hidden");
-        icon_menu.childNodes[5].classList.toggle("hidden");
-        icon_menu.childNodes[7].classList.toggle("hidden");
-        menu.children[0].classList.toggle("active");
-    }
-    if(user_icon.parentElement.parentElement.children[1].children[0].className === "active"){
-        user_icon.parentElement.parentElement.children[1].children[0].classList.toggle("active");
-        user_menu.classList.toggle("hidden");
-    }
+if (search_icon) search_icon.addEventListener('click', function () {
     search_input.classList.toggle("hidden");
     search_input.children[0].classList.toggle("active");
 })
 /* ================>>> Other search <<<=============== */
-if(other_search)other_search.addEventListener('click' , function(){
-    if(menu.children[0].className === "active"){
-        menu.classList.toggle("hidden");
-        icon_menu.childNodes[5].classList.toggle("hidden");
-        icon_menu.childNodes[7].classList.toggle("hidden");
-        menu.children[0].classList.toggle("active");
-    }
-    if(user_icon.parentElement.parentElement.children[1].children[0].className === "active"){
-        user_icon.parentElement.parentElement.children[1].children[0].classList.toggle("active");
-        user_menu.classList.toggle("hidden");
-    }
+if (other_search) other_search.addEventListener('click', function () {
     search_input.classList.toggle("hidden");
     search_input.children[0].classList.toggle("active");
 })
 
+document.addEventListener('click', (event) => {
+    if (search_icon && search_input) {
+        if (!search_icon.contains(event.target) && !search_input.contains(event.target)) {
+            search_input.classList.add('hidden');
+        }
+    }
+});
 /* ================>>> Focus Data <<<================= */
 let focus_data = document.querySelectorAll(".focus_data .data");
 let details = document.querySelectorAll(".view_Details img");
 
-if(focus_data)focus_data.forEach(function(ele){
-    ele.addEventListener('click' ,function(){
-        focus_data.forEach(function(focus){
+if (focus_data) focus_data.forEach(function (ele) {
+    ele.addEventListener('click', function () {
+        focus_data.forEach(function (focus) {
             focus.classList.remove("border-sky-500");
         })
-        details.forEach(function(img){
-            if(img.getAttribute("data") !== ele.getAttribute("focus")){
+        details.forEach(function (img) {
+            if (img.getAttribute("data") !== ele.getAttribute("focus")) {
                 img.classList.add("hidden");
-            }else{
+            } else {
                 img.classList.remove("hidden");
             }
         })
@@ -91,8 +75,8 @@ if(focus_data)focus_data.forEach(function(ele){
 
 /* ============>>> options <<<============== */
 let options = document.querySelectorAll(".options .option");
-options.forEach(function(option){
-    option.addEventListener('click' , function(){
+options.forEach(function (option) {
+    option.addEventListener('click', function () {
         this.children[1].classList.toggle("h-0");
     })
 })
