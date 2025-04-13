4ad1f104d3fa75712f25cec8de7475ace166df5d
Aly <el7amrawy@outlook.com>
2025-04-13 17:29:24 +0200

    edit pagination class

--- ELearning/Helpers/Pagination.cs
+++ ELearning/Helpers/Pagination.cs
@@ -8,6 +8,9 @@
         public int Pages => DivideAndRoundUp(Count, PageSize);
         public bool HasNext => PageNumber < Pages;
         public bool HasPrevious => PageNumber > 1;
+        public IEnumerable<int> NearbyPages => Enumerable
+        .Range(Math.Max(1, PageNumber - 2), Math.Min(5, Pages))
+        .Where(p => p >= 1 && p <= Pages);
         private int DivideAndRoundUp(int a, int b) => (a + b - 1) / b;
     }
 }
