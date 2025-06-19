8bdfddbf9294a2a79303573a49736c2f2d1ec8d7
Aly <el7amrawy@outlook.com>
2025-06-19 13:50:56 +0300

    add fota about page

---
+++ ELearning/Controllers/FotaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ELearning.Controllers
+{
+    public class FotaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
